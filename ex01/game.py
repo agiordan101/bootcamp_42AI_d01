4e9b369d01104729c6d2c53eb169f3d8dfacdb98
agiordan101 <agd611@gmail.com>
2020-05-26 23:01:56 +0200

    save ex02 fini

--- ex01/game.py
+++ ex01/game.py
@@ -3,35 +3,37 @@ class GotCharacter:
         self.first_name = first_name
         self.is_alive = is_alive
 
+
 class Stark(GotCharacter):
-    def __str__():
-        print("A class representing the Stark family.\
-        Or when bad things happen to good people.")
+    def __str__(self):
+        return "A class representing the Stark family.\
+        Or when bad things happen to good people."
 
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
         self.house_words = "Winter is Coming"
-    
-    def print_house_words():
+
+    def print_house_words(self):
         print("House words : {}".format(self.house_words), end="")
-    
-    def die():
+
+    def die(self):
         self.is_alive = False
 
+
 class Lannister(GotCharacter):
-    def __str__():
-        print("A class representing the Lannister family.")
+    def __str__(self):
+        return "A class representing the Lannister family."
 
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Lannister"
         self.house_words = "Les Lannister on est beau et tro for"
-    
-    def print_house_words():
+
+    def print_house_words(self):
         print("House words : {}".format(self.house_words), end="")
-    
-    def die():
+
+    def die(self):
         self.is_alive = False
 
 
@@ -41,6 +43,7 @@ joffrey = Lannister("Joffrey")
 print(str(arya))
 
 arya.print_house_words()
+print()
 print(arya.is_alive)
 arya.die()
 print(arya.is_alive)
--- ex02/vector.py
+++ ex02/vector.py
@@ -2,7 +2,12 @@ from numbers import Real
 
 
 class Vector:
+
+    def __str__(self):
+        return "Vector: {}".format(self.values)
+
     def __init__(self, param=None):
+        self.values = []
         if isinstance(param, list):
             for p in param:
                 self.values.append(float(p))
@@ -15,53 +20,86 @@ class Vector:
         else:
             print("Param <{}> is not float, list or tuple".format(param))
         self.size = len(self.values)
-    
-    def __add__(self, other)
+
+    def __add__(self, other):
         if isinstance(other, Vector):
             if self.size == other.size:
                 sum = []
-                for i in self.size:
+                for i in range(self.size):
                     sum.append(self.values[i] + other.values[i])
                 return Vector(sum)
             else:
-                print("Vector <{}> has not a size of {}".format(other, self.size))
+                print("Vector <{}> has not a size of {}".format(other,
+                      self.size))
         elif isinstance(other, Real):
             sum = []
-            for i in self.size:
+            for i in range(self.size):
                 sum.append(self.values[i] + other)
             return Vector(sum)
         else:
-            print("Can't add Vector with {}".format(other.type))
-    
+            print("Can't add Vector with {}".format(type(other)))
+
     def __radd__(self, other):
         return self + other
-    
-    def __sub__(self, other)
+
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return self + (-1 * other)
+        elif isinstance(other, Real):
+            return self + (-other)
+        else:
+            print("Can't sub a Vector with {}".format(type(other)))
+
+    def __rsub__(self, other):
+        if isinstance(other, Real):
+            return other + (-1 * self)
+        else:
+            print("Can't sub a Vector with {}".format(type(other)))
+
+    def __mul__(self, other):
         if isinstance(other, Vector):
             if self.size == other.size:
                 sum = []
-                for i in self.size:
-                    sum.append(self.values[i] - other.values[i])
+                for i in range(self.size):
+                    sum.append(self.values[i] * other.values[i])
                 return Vector(sum)
             else:
-                print("Vector <{}> has not a size of {}".format(other, self.size))
+                print("Vector <{}> has not a size of {}".format(other,
+                      self.size))
         elif isinstance(other, Real):
             sum = []
-            for i in self.size:
-                sum.append(self.values[i] - other)
+            for i in range(self.size):
+                sum.append(self.values[i] * other)
             return Vector(sum)
         else:
-            print("Can't sub a Vector with {}".format(other.type))
-    
-    def __rsub__(self, other):
-        if isinstance(other, Vector):
-            return other - self
-        elif isinstance(other, Real):
+            print("Can't mult Vector with {}".format(type(other)))
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, Real):
+            if other == 0:
+                print("Can't div by 0 !")
+            else:
+                return self * (1.0 / other)
+        else:
+            print("Can't div Vector with {}".format(type(other)))
+
+    def __rtruediv__(self, other):
+        if isinstance(other, Real):
             sum = []
-            for i in self.size:
-                sum.append(other - self.values[i])
+            for value in self.values:
+                if value == 0:
+                    print("Can't div by 0 !")
+                    return None
+                else:
+                    sum.append(other / value)
             return Vector(sum)
         else:
-            print("Can't sub a Vector with {}".format(other.type))
-    
-    
+            print("Can't div {} by a Vector".format(type(other)))
+
+    def __repr__(self):
+        print(str(self))
+        print("Dimension: {}x1".format(self.size))
+        return self.values
